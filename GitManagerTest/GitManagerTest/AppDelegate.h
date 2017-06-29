88fffaa007428da87b10252ac26a085a649b8279
lei qiao <qiaolei920731@icloud.com>
2017-06-29 18:32:04 +0800

    代码库

---
+++ GitManagerTest/GitManagerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitManagerTest
+//
+//  Created by jgs on 2017/6/29.
+//  Copyright © 2017年 QL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
